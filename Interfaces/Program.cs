5a7daffad546c7890e06803d9b11a11a05e7bce3
fatih emre <yeni.udemy@gmail.com>
2024-02-25 20:33:45 +0300

    ilk commit

---
+++ Interfaces/Program.cs
@@ -0,0 +1,83 @@
+﻿namespace Interfaces
+{
+    internal class Program
+    {
+        //interface new lenemez
+        static void Main(string[] args)
+        {
+            //IPersonManager customerManager = new CustomerManager();
+            //customerManager.Add();
+
+            //IPersonManager employeeManager = new EmployeeManager();
+            //employeeManager.Add();
+
+            ProjectManager projectManager = new ProjectManager();
+            projectManager.Add(new InternManager());
+
+        }
+    }
+
+    interface IPersonManager
+    {
+        //unimplemented operation
+        void Add();
+
+        void Update();
+
+    }
+
+    class CustomerManager : IPersonManager
+    {
+        public void Add()
+        {
+            //müşteri ekleme kodları
+            Console.WriteLine("Müşteri eklendi");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Müşteri güncellendi");
+        }
+    }
+
+    class EmployeeManager : IPersonManager
+    {
+        public void Add()
+        {
+            //personel ekleme kodları
+            Console.WriteLine("personel eklendi");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("personel güncellendi");
+
+        }
+    }
+
+    class InternManager : IPersonManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Stajyer eklendi");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Stajyer güncellendi");
+        }
+    }
+
+
+    class ProjectManager
+    {
+        public void Add(IPersonManager personManager)
+        {
+            personManager.Add();
+        }
+    }
+
+
+
+
+}
